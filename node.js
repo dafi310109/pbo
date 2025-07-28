a4e022b6c875fa463dd612a0c7d2ca1b5b598623
dafi310109 <alfajardafi77@gmail.com>
2025-07-28 14:59:35 +0700

    latihan java script

---
+++ node.js
@@ -0,0 +1,23 @@
+// 
+
+let bike = {
+    merk: "polygon",
+    jumgear: 20,
+    color: "black",
+
+    start: function() {
+        console.log("sepeda mulai dikayuh.")
+    },
+    stop: function() {
+        console.log("sepeda mengerem.")
+    },
+    
+};
+
+console.log(bike.merk);
+console.log(bike.jumgear);
+console.log(bike.color);
+
+
+bike.start();
+bike.stop();i45
